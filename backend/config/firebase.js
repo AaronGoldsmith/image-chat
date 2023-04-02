3fe6a335b60f95a353d12c6efd89719564ea31f4
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2023-04-02 09:56:41 -0700

    - add bulma, update guest info, update feed item

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -4,6 +4,7 @@ const serviceAccount = require(`../${process.env.GOOGLE_APPLICATION_CREDENTIALS}
 try {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'imagegen-36210.appspot.com'
   });
 } catch (err) {
   console.error('Firebase initialization error', err.stack);
--- frontend/src/components/User/GuestInfo.js
+++ frontend/src/components/User/GuestInfo.js
@@ -6,9 +6,11 @@ const GuestInfo = ({updateLoggedInUser}) => {
 
   return (
     <div className="user-info">
-      <img className="user-photo" src="https://picsum.photos/200/200.jpg" alt="" />
-      <div className="user-details">
-        <h2 className="display-name">Guest</h2>
+      <div className="user-info-content">
+        <img className="user-photo" src="https://picsum.photos/200/200.jpg" alt="" />
+        <div className="user-details">
+          <h2 className="display-name">Guest</h2>
+        </div>
       </div>
       <LoginWithGoogle updateLoggedInUser={user => updateLoggedInUser(user)}/>
     </div>
